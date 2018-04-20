f396f7089ff93c9a7f2ccbd2e370bdfe234acf25
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-04-20 13:15:31 +0700

    :umbrella: :penguin: main app testing

--- app/src/androidTest/java/com/yoesuv/mvpnetworking/MainActivityTest.java
+++ app/src/androidTest/java/com/yoesuv/mvpnetworking/MainActivityTest.java
@@ -1,9 +1,12 @@
 package com.yoesuv.mvpnetworking;
 
+import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
 
 import com.yoesuv.mvpnetworking.main.MainActivity;
 
@@ -12,6 +15,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressBack;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
@@ -22,17 +30,42 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 @LargeTest
 public class MainActivityTest {
 
+    UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
     @Rule
     public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void clickMenuListPlace(){
+    public void startApplication(){
         Espresso.onView(withId(R.id.buttonListPlace)).perform(click());
-    }
-
-    @Test
-    public void clickMenuGallery(){
+        // ===== open list place =====
+        SystemClock.sleep(2000);
+        Espresso.onView(withId(R.id.swipeRefreshListPlace)).perform(TestHelper.withCustomConstraints(swipeDown(), isDisplayingAtLeast(50)));
+        SystemClock.sleep(2000);
+        Espresso.onView(withId(R.id.recyclerViewListPlace)).perform(swipeUp());
+        Espresso.onView(withId(R.id.recyclerViewListPlace)).perform(swipeUp());
+        Espresso.onView(withId(R.id.recyclerViewListPlace)).perform(swipeUp());
+        Espresso.onView(withId(R.id.recyclerViewListPlace)).perform(swipeUp());
+        SystemClock.sleep(1000);
+        Espresso.onView(withId(R.id.recyclerViewListPlace)).perform(swipeUp());
+        SystemClock.sleep(1000);
+        Espresso.onView(withId(R.id.recyclerViewListPlace)).perform(swipeDown());
+        SystemClock.sleep(2000);
+        device.pressBack();
+        SystemClock.sleep(500);
+        //===== open list gallery =====
         Espresso.onView(withId(R.id.buttonGallery)).perform(click());
+        SystemClock.sleep(2000);
+        Espresso.onView(withId(R.id.swipeRefreshGallery)).perform(TestHelper.withCustomConstraints(swipeDown(), isDisplayingAtLeast(50)));
+        SystemClock.sleep(2000);
+        Espresso.onView(withId(R.id.recyclerViewGallery)).perform(swipeUp());
+        Espresso.onView(withId(R.id.recyclerViewGallery)).perform(swipeUp());
+        SystemClock.sleep(500);
+        Espresso.onView(withId(R.id.recyclerViewGallery)).perform(swipeUp());
+        SystemClock.sleep(1000);
+        Espresso.onView(withId(R.id.recyclerViewGallery)).perform(swipeDown());
+        device.pressBack();
+        SystemClock.sleep(500);
     }
 
 }
